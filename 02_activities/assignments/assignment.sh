26a88c7ad71d144e8fcea34138fb12a1306fc906
Arman <aakavalekar@gmail.com>
2025-08-03 13:57:01 -0400

    Answering all part 1 questions take 8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,7 +52,6 @@ rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd ./data
 ls ./data/processed/* > ./data/inventory.txt
 
 ###########################################
